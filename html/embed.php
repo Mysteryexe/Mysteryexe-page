a1b6bd6c9b5b2f6cfa5fc88d76b70d8288ceaeeb
Mysteryexe <99576711+Mysteryexe@users.noreply.github.com>
2022-02-13 09:16:07 +0330

    css

---
+++ html/embed.php
@@ -0,0 +1,11 @@
+<?php
+echo '
+<meta name="theme-color" content="#FF6500">
+<meta property="og:type" content="Website">
+<meta property="og:image" content="content/3Dpfp.gif">
+<meta property="og:site_name" content="Mystery Studio Support">
+<meta property="og:title" content="Never gonna give you up">
+<meta property="og:description" content="TEST TEXT">
+<meta property="og:url" content="https://mysterysupport.rf.gd">
+';
+?>
