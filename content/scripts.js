65bf24c22ba5f568e7ce89dbca938dcf9b952e9f
Mysteryexe <99576711+Mysteryexe@users.noreply.github.com>
2022-05-17 12:26:41 +0430

    Update scripts.js

--- content/scripts.js
+++ content/scripts.js
@@ -1,6 +1,7 @@
 num = 0;
 var i = 1;
 var lastPrj;
+var curImage = 1;
 document.addEventListener("mousemove", parallax);
 function parallax(event) {
   this.querySelectorAll(".bgTexture, .mouse").forEach((shift) => {
@@ -19,12 +20,6 @@ function betterPerf() {
   setTimeout(function () {
     var y = document.getElementsByClassName("bgTexture");
     var elems1 = document.getElementsByClassName("bgcontainer");
-    var elems2 = document.getElementsByTagName("inproject");
-    for (var i = 0; i < elems2.length; i += 1) {
-      if (elems2[i].style.opacity <= 0.05) {
-        elems2[i].style.visibility = "hidden";
-      }
-    }
     var offset = elems1[0].offsetHeight;
     if (window.scrollY > offset) {
       y[0].style.webkitAnimationPlayState = "paused";
@@ -38,84 +33,55 @@ function betterPerf() {
   }, 3000);
 }
 betterPerf();
-function resizesvg(y) {
-  var elems = y.getElementsByTagName("inproject");
-  var elems2 = document.getElementsByTagName("inproject");
-  var elems3 = y.getElementsByTagName("h3");
-  var elems4 = document.getElementsByTagName("h3");
-  for (var i = 0; i < elems2.length; i += 1) {
-    elems2[i].style.opacity = "0";
-  }
-  for (var i = 0; i < elems4.length; i += 1) {
-    elems4[i].style.opacity = "1";
-  }
-  for (var i = 0; i < elems.length; i += 1) {
-    elems[i].style.visibility = "visible";
-    elems[i].style.opacity = "1";
-  }
-  for (var i = 0; i < elems3.length; i += 1) {
-    elems3[i].style.opacity = "0";
-  }
-}
-function ChangeImage(x, status) {
-  if (x == "prj1") {
-    var a = [
-      "content/images/RenderFinalWithMarker.webp",
-      "content/images/endmsg.webp",
-    ];
-  }
-  if (x == "prj2") {
-    var a = [
-      "content/images/Gingernium01.webp",
-      "content/images/Gingernium02.webp",
-      "content/images/Gingernium03.webp",
-      "content/images/Gingernium04.webp",
-      "content/images/Gingernium05.webp",
-      "content/images/Gingernium06.webp",
-      "content/images/endmsg.webp",
-    ];
-  }
-  if (x == "prj3") {
-    var a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  }
-  if (x == "prj4") {
-    var a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  }
-  if (lastPrj != x) {
-    num = 0;
-  }
-  if ((num > -1, num < a.length)) {
-    if (status == "back") {
-      num = num - 1;
-      if (num == -1) {
-        num = num + 1;
-      }
-    }
-    if (status == "next") {
-      num = num + 1;
-      if (num == a.length) {
-        num = num - 1;
-      }
+function ChangeImage(image, status) {
+  const projectImg = document.getElementsByClassName("projectimg");
+  var index = 0;
+  const images = ["render", "gingernium", "soon1", "soon2"];
+  const render = [
+    "content/images/RenderFinalWithMarker.webp",
+    "content/images/endmsg.webp",
+  ];
+  const gingernium = [
+    "content/images/Gingernium01.webp",
+    "content/images/Gingernium02.webp",
+    "content/images/Gingernium03.webp",
+    "content/images/Gingernium04.webp",
+    "content/images/Gingernium05.webp",
+    "content/images/Gingernium06.webp",
+    "content/images/endmsg.webp",
+  ];
+  const soon1 = ["content/svg/soon.svg"];
+  const soon2 = ["content/svg/soon.svg"];
+  const links = [render, gingernium, soon1, soon2];
+  for (var i = 0; i < images.length; i += 1) {
+    if (images[i] == image) {
+      index = i;
     }
   }
-  const y = document.getElementById(x);
-  const z = y.getElementsByTagName("inproject");
-  const t = z[0].getElementsByClassName("projectimg");
-  const text = z[0].getElementsByTagName("p3");
-  if (a[num] == "content/images/endmsg.webp") {
-    text[0].style.visibility = "hidden";
-    text[0].style.opacity = 0;
+  const curlinks = links[index];
+  if (curImage > curlinks.length) {
+    curImage = 0;
+  }
+  const text = document.getElementsByTagName("p3")[index];
+  projectImg[index].src = String(links[index][curImage]);
+  if (status == "next" && curImage < curlinks.length - 1) {
+    curImage += 1;
+    projectImg[index].src = String(links[index][curImage]);
+  } else if (status == "back" && curImage > 0) {
+    curImage -= 1;
+    projectImg[index].src = String(links[index][curImage]);
+  }
+  if (links[index][curImage] == "content/images/endmsg.webp") {
+    text.style.opacity = 0;
   } else {
-    text[0].style.visibility = "visible";
-    text[0].style.opacity = 1;
+    text.style.opacity = 1;
   }
-  t[0].src = a[num];
-  lastPrj = x;
+  lastPrj = index;
 }
 function openEffect(img) {
   const images = ["render", "gingernium", "soon1", "soon2"];
   const icon = document.getElementsByClassName(img);
-  icon[0].style.transform = "scale(10) rotate(720deg)";
+  icon[0].style.transform = "translatey(-120%)";
   for (var i = 0; i < images.length; i += 1) {
     if (images[i] == img) {
     } else {
@@ -124,75 +90,30 @@ function openEffect(img) {
     }
   }
 }
-function PrjAction1() {
-  openEffect("render");
+function PrjAction(x) {
+  const images = ["render", "gingernium", "soon1", "soon2"];
+  const projects = document.getElementsByClassName("projectdiv");
   if ($(window).width() < 900) {
-    const x = document.getElementById("prj1");
-    x.style.width = "90%";
-    document.getElementById("prj2").style.width = "30%";
-    document.getElementById("prj3").style.width = "30%";
-    document.getElementById("prj4").style.width = "30%";
-    resizesvg(x);
+    var small = "30%";
+    var big = "90%";
   } else {
-    const x = document.getElementById("prj1");
-    x.style.width = "50%";
-    document.getElementById("prj2").style.width = "15%";
-    document.getElementById("prj3").style.width = "15%";
-    document.getElementById("prj4").style.width = "15%";
-    resizesvg(x);
+    var small = "15%";
+    var big = "45%";
   }
-}
-function PrjAction2() {
-  openEffect("gingernium");
-  if ($(window).width() < 900) {
-    document.getElementById("prj1").style.width = "30%";
-    const x = document.getElementById("prj2");
-    x.style.width = "90%";
-    document.getElementById("prj3").style.width = "30%";
-    document.getElementById("prj4").style.width = "30%";
-    resizesvg(x);
-  } else {
-    document.getElementById("prj1").style.width = "15%";
-    const x = document.getElementById("prj2");
-    x.style.width = "50%";
-    document.getElementById("prj3").style.width = "15%";
-    document.getElementById("prj4").style.width = "15%";
-    resizesvg(x);
-  }
-}
-function PrjAction3() {
-  openEffect("soon1");
-  if ($(window).width() < 900) {
-    document.getElementById("prj1").style.width = "30%";
-    document.getElementById("prj2").style.width = "30%";
-    const x = document.getElementById("prj3");
-    x.style.width = "90%";
-    document.getElementById("prj4").style.width = "30%";
-    resizesvg(x);
-  } else {
-    document.getElementById("prj1").style.width = "15%";
-    document.getElementById("prj2").style.width = "15%";
-    const x = document.getElementById("prj3");
-    x.style.width = "50%";
-    document.getElementById("prj4").style.width = "15%";
-    resizesvg(x);
-  }
-}
-function PrjAction4() {
-  openEffect("soon2");
-  if ($(window).width() < 900) {
-    document.getElementById("prj1").style.width = "30%";
-    document.getElementById("prj2").style.width = "30%";
-    document.getElementById("prj3").style.width = "30%";
-    const x = document.getElementById("prj4");
-    x.style.width = "90%";
-    resizesvg(x);
-  } else {
-    document.getElementById("prj1").style.width = "15%";
-    document.getElementById("prj2").style.width = "15%";
-    document.getElementById("prj3").style.width = "15%";
-    const x = document.getElementById("prj4");
-    x.style.width = "50%";
-    resizesvg(x);
+
+  openEffect(x);
+  for (var i = 0; i < images.length; i += 1) {
+    if (images[i] == x) {
+      if (projects[i].style.width != big) {
+        ChangeImage(images[i], "back");
+      }
+      projects[i].style.width = big;
+      var imageContainer = projects[i].querySelectorAll("inproject");
+      imageContainer[0].style.transform = "translatey(0%)";
+    } else {
+      projects[i].style.width = small;
+      var imageContainer = projects[i].querySelectorAll("inproject");
+      imageContainer[0].style.transform = "translatey(120%)";
+    }
   }
 }
