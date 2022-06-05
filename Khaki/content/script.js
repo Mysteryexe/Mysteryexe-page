ae29ab041aa2c72f519850d1524565e95eb888c0
Mysteryexe <99576711+Mysteryexe@users.noreply.github.com>
2022-06-05 12:15:35 +0430

    Add files via upload

--- Khaki/content/script.js
+++ Khaki/content/script.js
@@ -1,9 +1,9 @@
 function navbarTran() {
   setTimeout(function () {
-    navbar = document.getElementById("navbar");
+    const navbar = document.getElementById("navbar");
     if (window.scrollY > navbar.offsetHeight) {
       navbar.style.backgroundColor = "#c2ded10";
-      navbar.style.backdropFilter = "blur(1px)";
+      navbar.style.backdropFilter = "blur(0.15vh)";
     } else {
       navbar.style.backgroundColor = " #c2ded100";
       navbar.style.backdropFilter = "blur(0px)";
@@ -11,8 +11,18 @@ function navbarTran() {
     navbarTran();
   }, 600);
 }
+function smoothScroll() {
+  navbar = document.getElementById("navbar");
+  const navheight = navbar.offsetHeight;
+  console.log(navheight);
+  document.documentElement.style.setProperty(
+    "--scroll-padding",
+    navheight - 1 + "px"
+  );
+}
 function load() {
   navbarTran();
+  smoothScroll();
   //toggle mobile menu
   const clickx = document.getElementById("pencet");
   const navbar = document.getElementsByTagName("nav")[0];
